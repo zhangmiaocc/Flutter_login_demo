34c9c90990f0e1f22ed322af73d1cdff5f9e78ca
zhangmiao <479326215@qq.com>
2019-05-28 18:36:24 +0800

    no message

---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TextFieldAndCheckPage(),
+    );
+  }
+}
+
+class TextFieldAndCheckPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => TextFieldAndCheckPageState();
+}
+
+class TextFieldAndCheckPageState extends State<TextFieldAndCheckPage> {
+  //手机号的控制器
+  TextEditingController phoneController = TextEditingController();
+
+  //密码的控制器
+  TextEditingController passController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('登录'),
+      ),
+      body: Column(
+        children: <Widget>[
+          TextField(
+            controller: phoneController,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(10.0),
+              icon: Icon(Icons.phone),
+              labelText: '请输入你的用户名)',
+              helperText: '请输入注册的手机号',
+            ),
+            autofocus: false,
+          ),
+          TextField(
+              controller: passController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(10.0),
+                icon: Icon(Icons.lock),
+                labelText: '请输入密码)',
+              ),
+              obscureText: true),
+          RaisedButton(
+            onPressed: _login,
+            child: Text('登录'),
+          ),
+          RaisedButton(
+            onPressed: onTextClear,
+            child: Text('清除'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _login() {
+    print({'phone': phoneController.text, 'password': passController.text});
+    if (phoneController.text.length != 11) {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('手机号码格式不对'),
+          ));
+    } else if (passController.text.length == 0) {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('请填写密码'),
+          ));
+    } else {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('登录成功'),
+          ));
+      phoneController.clear();
+    }
+  }
+
+  void onTextClear() {
+    setState(() {
+      phoneController.clear();
+      passController.clear();
+    });
+  }
+}
